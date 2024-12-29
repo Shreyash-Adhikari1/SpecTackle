bbeefce39bc0669493c2c17e34860aa094359550
Arti Manandhar <artimanandhar@gmail.com>
2024-12-29 10:45:45 +0545

    category test

--- app/src/main/java/com/example/spectackle/category/CategoryPage.kt
+++ app/src/main/java/com/example/spectackle/category/CategoryPage.kt
@@ -1,10 +1,11 @@
-package com.example.spectackle
+package com.example.spectackle.category
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.spectackle.R
 
 class CategoryPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
