90d7e86ece9b813ebbfb1c52a7d95b3404a27640
Shreyash-Adhikari1 <adhikarishreyash@gmail.com>
2025-01-27 09:47:24 +0545

    Manifest Error fix

--- app/src/main/java/com/example/spectackle/ui/fragment/CartFragment.kt
+++ app/src/main/java/com/example/spectackle/ui/fragment/CartFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import com.example.spectackle.R
 import com.example.spectackle.databinding.FragmentCartBinding
+import com.example.spectackle.ui.activity.home.HomeActivity
 
 
 class CartFragment : Fragment() {
@@ -20,16 +21,17 @@ class CartFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentCartBinding.inflate(inflater,container,false)
+        binding = FragmentCartBinding.inflate(inflater,container,false)// maathi parameter ma jj xa tehi pass garney
         return binding.root
     }
 
+    //code chahi yes bhitra garney
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        Toast.makeText(requireContext()) //this@ garera context haalnu mildaina so requireContext garnu parxa
+        Toast.makeText(requireContext (CartFragment),"Bruhh",Toast.LENGTH_LONG).show() //this@ garera context haalnu mildaina so requireContext garnu parxa
 
-        val intent = Intent(requireContext(),)//just in-case requireContext ma error aayo bhaney requireActivity garney
+        val intent = Intent(requireActivity())//just in-case requireContext ma error aayo bhaney requireActivity garney
 
     }
 
