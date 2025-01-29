c5b6cacb6dfc50370ef767369b186efa1b741b6e
Shreyash-Adhikari1 <adhikarishreyash@gmail.com>
2025-01-29 10:01:30 +0545

    Adapter Build || REcycler View done

--- app/src/main/java/com/example/spectackle/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/spectackle/ui/fragment/HomeFragment.kt
@@ -5,12 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.example.spectackle.R
+import com.example.spectackle.adapter.HomeProductsAdapter
 import com.example.spectackle.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
     lateinit var binding: FragmentHomeBinding
+    lateinit var recyclerView: RecyclerView
+
+    var imageList=ArrayList<Int>()
+    var nameList=ArrayList<String>()
+    var priceList=ArrayList<String>()
+    lateinit var adapter: HomeProductsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,7 +30,7 @@ class HomeFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
+        //imageList.add()
 
     }
 }
